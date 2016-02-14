3fe240cf3ed41d4e89bcf7bd82613891d389f704
nesticle8bit <juliojafeth2000_@hotmail.com>
2016-02-14 16:15:32 -0500

    New CRUD for Sectors

---
+++ News System/Controllers/SectorController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using News_System.Models;
+
+namespace News_System.Controllers
+{
+    public class SectorController : Controller
+    {
+        private newsSystem_dbEntities db = new newsSystem_dbEntities();
+
+        // GET: Sector
+        public ActionResult Index()
+        {
+            return View(db.Sector.OrderBy(o => o.Name).ToList());
+        }
+
+        // GET: Sector/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sector sector = db.Sector.Find(id);
+            if (sector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sector);
+        }
+
+        public JsonResult DetailsSector(int? id)
+        {
+            var data = (from c in db.Sector
+                        where c.Id == id
+                        select new
+                        {
+                            Id = c.Id,
+                            Name = c.Name
+                        }).SingleOrDefault();
+
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Sector/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sector/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Sector sector)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sector.Add(sector);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sector);
+        }
+
+        // GET: Sector/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sector sector = db.Sector.Find(id);
+            if (sector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sector);
+        }
+
+        // POST: Sector/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Sector sector)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sector).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sector);
+        }
+
+        // GET: Sector/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sector sector = db.Sector.Find(id);
+            if (sector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sector);
+        }
+
+        // POST: Sector/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sector sector = db.Sector.Find(id);
+            db.Sector.Remove(sector);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
